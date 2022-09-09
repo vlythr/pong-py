5de3fb2095936ef3c75ef81dc36ac2c9a1db3042
vlythr <vlythr@pm.me>
2022-09-09 16:25:33 -0300

    imported modules and added game window configuration

---
+++ pong.py
@@ -0,0 +1,18 @@
+import turtle
+import os
+
+wd = turtle.Screen()
+wd.title("Pong by @vlythr")
+wd.bgcolor("purple")
+wd.setup(width=800, height=600)
+wd.tracer(0)
+
+
+
+# Main game loop
+while True:
+    wd.update()
+    
+
+
+
