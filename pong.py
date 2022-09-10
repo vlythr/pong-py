24cc7fc221160aa02700b9d914cec91aea3cf4c8
vlythr <vlythr@pm.me>
2022-09-09 22:04:47 -0300

    Added and defined parameters for paddles

--- pong.py
+++ pong.py
@@ -7,6 +7,21 @@ wd.bgcolor("purple")
 wd.setup(width=800, height=600)
 wd.tracer(0)
 
+# Right Paddle
+right_p = turtle.Turtle()
+right_p.speed(0)
+right_p.shape("square")
+right_p.color("green")
+right_p.penup()
+right_p.goto(-350, 0)
+
+# Left Paddle
+left_p = turtle.Turtle()
+left_p.speed(0)
+left_p.shape("square")
+left_p.color("green")
+left_p.penup()
+left_p.goto(350, 0)
 
 
 # Main game loop
