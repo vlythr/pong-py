2fc23ba52d72eb9fc6b39bc30a031a5a9e72bd51
vlythr <vlythr@pm.me>
2022-09-09 22:09:07 -0300

    Added ball and fixed paddles shapesize

--- pong.py
+++ pong.py
@@ -12,6 +12,7 @@ right_p = turtle.Turtle()
 right_p.speed(0)
 right_p.shape("square")
 right_p.color("green")
+right_p.shapesize(stretch_wid=6, stretch_len=1)
 right_p.penup()
 right_p.goto(-350, 0)
 
@@ -20,8 +21,17 @@ left_p = turtle.Turtle()
 left_p.speed(0)
 left_p.shape("square")
 left_p.color("green")
+left_p.shapesize(stretch_wid=6, stretch_len=1)
 left_p.penup()
 left_p.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("green")
+ball.penup()
+ball.goto(0, 0)
 
 
 # Main game loop
